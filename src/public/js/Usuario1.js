d207182c9f4f476b816cab0ea6630c5e343ce5f2
Mónica Alvarado <monicalucia.alvarado@gmail.com>
2025-04-21 02:06:59 -0600

    reactivo

--- src/public/js/Usuario1.js
+++ src/public/js/Usuario1.js
@@ -2,46 +2,53 @@ const contenedores = document.querySelectorAll('.contenedor-imagen');
 
 const selectDenuncia = document.getElementById('denuncia-usuario');
 
-    const denuncias = document.querySelectorAll('.denuncia-individual');
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.ulMenu');
 
-    contenedores.forEach((contenedor) => {
-        const imagen = contenedor.querySelector('.imagen-denuncia');
-        const imagenes = contenedor.getAttribute('data-images') ? contenedor.getAttribute('data-images').split(',') : []; // Obtenemos las imágenes de 'data-images' como array (con comprobación de existencia)
-        let currentImageIndex = 0;
+const denuncias = document.querySelectorAll('.denuncia-individual');
 
-        if (imagenes.length === 0) {
-            console.warn('El contenedor no tiene imágenes asociadas');
-            return;
-        }
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('hidden2');
+});
 
-        contenedor.addEventListener('click', function () {
-            currentImageIndex = (currentImageIndex + 1) % imagenes.length; // Incrementa y vuelve a 0 al llegar al final
-            imagen.src = imagenes[currentImageIndex];
-        });
+contenedores.forEach((contenedor) => {
+    const imagen = contenedor.querySelector('.imagen-denuncia');
+    const imagenes = contenedor.getAttribute('data-images') ? contenedor.getAttribute('data-images').split(',') : []; // Obtenemos las imágenes de 'data-images' como array (con comprobación de existencia)
+    let currentImageIndex = 0;
 
-        contenedor.addEventListener('contextmenu', function (e) {
-            e.preventDefault();
-            currentImageIndex = (currentImageIndex - 1 + imagenes.length) % imagenes.length; // Decrementa y vuelve al final si llega a 0
-            imagen.src = imagenes[currentImageIndex];
-        });
+    if (imagenes.length === 0) {
+        console.warn('El contenedor no tiene imágenes asociadas');
+        return;
+    }
+
+    contenedor.addEventListener('click', function () {
+        currentImageIndex = (currentImageIndex + 1) % imagenes.length; // Incrementa y vuelve a 0 al llegar al final
+        imagen.src = imagenes[currentImageIndex];
     });
 
-    selectDenuncia.addEventListener('change', function () {
+    contenedor.addEventListener('contextmenu', function (e) {
+        e.preventDefault();
+        currentImageIndex = (currentImageIndex - 1 + imagenes.length) % imagenes.length; // Decrementa y vuelve al final si llega a 0
+        imagen.src = imagenes[currentImageIndex];
+    });
+});
+
+selectDenuncia.addEventListener('change', function () {
+    denuncias.forEach(denuncia => {
+        denuncia.classList.add('oculto');
+        denuncia.classList.remove('mostrar');
+    });
+
+    const seleccionada = selectDenuncia.value;
+
+    if (seleccionada == "Todas") {
         denuncias.forEach(denuncia => {
-            denuncia.classList.add('oculto');
-            denuncia.classList.remove('mostrar');
+            denuncia.classList.remove('oculto');
+            denuncia.classList.add('mostrar');
         });
-
-        const seleccionada = selectDenuncia.value;
-
-        if(seleccionada=="Todas"){
-            denuncias.forEach(denuncia => {
-                denuncia.classList.remove('oculto');
-                denuncia.classList.add('mostrar');
-            });
-        } else{
-            const denunciaSeleccionada = document.getElementById(`denuncia-usuario${selectDenuncia.selectedIndex}`);
-            denunciaSeleccionada.classList.remove('oculto');
-            denunciaSeleccionada.classList.add('mostrar');
-        }
-    });
+    } else {
+        const denunciaSeleccionada = document.getElementById(`denuncia-usuario${selectDenuncia.selectedIndex}`);
+        denunciaSeleccionada.classList.remove('oculto');
+        denunciaSeleccionada.classList.add('mostrar');
+    }
+});
